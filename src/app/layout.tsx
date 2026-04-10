111c1ad22096d36aa76aa8217f1ee1e98cd18305
Ryan da Silva <profissionalryan68@gmail.com>
2026-04-10 16:45:11 -0300

    fix: remove .tsx extension from Footer import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Header from "../components/Header";
-import Footer from "../components/Footer.tsx";
+import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
   return (
